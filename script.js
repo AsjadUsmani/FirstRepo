3799ed509ed30e9a7614f9e6ffa526120a98a1dd
AsjadUsmani <156511939+AsjadUsmani@users.noreply.github.com>
2024-03-04 22:49:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ function setActiveLink(linkId) {
 }
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
